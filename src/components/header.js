f4632fefa8109e2854ecfdab35ad45baca5f2be9
Jordy Vazquez <jordyvazquez@Jordys-MBP.home>
2021-03-17 22:14:28 -0600

    finished adding the header and styles to it.

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className="header-container">
                 Redis Link Shortener
             </div>
         );
